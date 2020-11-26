001acc7ddc293c91c23edd8805dff63d45d9a3a4
Andrew SzeTo <zkwokleung@gmail.com>
2020-11-26 23:26:53 +0800

    FIX: Input functions

--- Input.h
+++ Input.h
@@ -6,20 +6,48 @@
 // Get the character input from the player and process it to int between 0 ~ 9
 int getUserInput()
 {
-    char *ipt;
+    char ipt;
     printf("> ");
-    fgets(ipt, 2, stdin);
+    fgets(&ipt, 2, stdin);
+
+    int result = ipt - '0';
+    // printf("Debug: you entered %d", result);
 
     // Verify the Input
-    if (*ipt >= 1 && *ipt <= 9)
+    if (result >= 0 && result <= 9)
     {
-        return *ipt - '0';
+        return result;
     }
 
     // Invalid Input.
     // Recursively ask the player until there is a valid input
     printf("/// Invalid input! Input must between 0 and 9! /// \n");
     return getUserInput();
+}
+
+// Get confirmation from the user
+// Y: return 1; N: return 0; other: Ask again
+int getUserConfirm()
+{
+    char ipt;
+
+    printf("Are you sure? (Y/N) > ");
+    fgets(&ipt, 2, stdin);
+
+    switch (ipt)
+    {
+    case 'Y':
+    case 'y':
+        return 1;
+
+    case 'N':
+    case 'n':
+        return 0;
+
+    default:
+        printf("/// Invalid input! ///\n");
+        return getUserConfirm();
+    }
 }
 
 #endif // !Input
