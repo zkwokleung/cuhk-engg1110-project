68cba2b97f9f6d8507451ae063dcb56ca176e612
Andrew SzeTo <zkwokleung@gmail.com>
2020-11-26 20:41:39 +0800

    ADDED: Temporary Menu.h

---
+++ Menu.h
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#ifndef MENU
+#define MENU
+// Main Menu
+void printMainMenu()
+{
+    printf("==============================\n");
+    printf("||  Welcome to my project!  ||\n");
+    printf("==============================\n");
+    printf("|| Choose and option:       ||\n");
+    printf("||  [1] New game            ||\n");
+    printf("||  [2] Load game           ||\n");
+    printf("||  [0] Exit                ||\n");
+}
+
+// New Game MENU
+void printNewGameMenu()
+{
+    printf("==============================\n");
+    printf("||        Game Mode         ||\n");
+    printf("==============================\n");
+    printf("|| Choose and option:       ||\n");
+    printf("||  [1] Single Player       ||\n");
+    printf("||  [2] Multiplayer         ||\n");
+    printf("||  [0] Back                ||\n");
+}
+#endif // !MENU
